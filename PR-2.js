7a8ea50479181d06889263aba1beac4ecb38015a
Swati-11 <Swatinaama@gmail.com>
2020-05-25 21:27:23 +0530

    Changes in .js and .css .uploaded images folder

--- PR-2.js
+++ PR-2.js
@@ -1,54 +1,46 @@
-  
-  var playing = false;
-  var score =0;  
-  var trailsleft;
+var playing = false;
+var score =0;  
+var trailsleft;
+var chooseFruit=["apple","banana","watermelon","mango","cherries","orange","grapes","pear","peach"];
 
 $(function(){
+  $("#start-reset").click(function(){                  //click on start-reset button
   
-  $("#start-reset").click(function(){
-    if (playing == true){
-                    //click on start-reset button
-                    // if playing
-                    //yes
-                    //reload the page
-        
-        location.reload();
-        
-    }
-    else{ //no
-        
+      if (playing == true){                            // if playing       //yes
+          location.reload();                           //reload the page
+         }
+   
+      else{                                             //no
         playing = true;
         $("#score-value").html(score); 
-        $("#trails").show();
         trailsleft=3;
-                       addhearts();
-                        //show trails left
-                        //start game to reset game
-                        //1. create a random fruit
-                        //define a random step
-                        //2.move fruit one step down
-                        //is the fruit too low?
-                        //no?
-                        //repeat step-2
-                        // 
-           
-    }
+        document.getElementById("trails").style.visibility= "visible";                  //show trails left
+        addHearts();
+                        
+        $("#start-reset").html("RESET");                                                //start game to reset game  
+          
+        $("#fruits").show();                                                            //1. create a random fruit
+        showFruits();
+                       
+                                                                                        //define a random step
+                                                                                        //2.move fruit one step down
+                                                                                        //is the fruit too low?
+                                                                                        //no?
+                                                                                        //repeat step-2
+                                                                                        // 
+            }
+    });
 });
 
-    });
-
-function addhearts(){
-      for(i =0;i<trailsleft;i++){
-          $("#trails").append(" X ");
-      }
+function addHearts(){
+     for(i =0;i<trailsleft;i++){
+           $("#trails").append( '<img src="images/heart.png" class="heart">');
+         }
   } 
-     
 
-  //click on start-reset button
-
-
-
-// if playing
-    //yes
-        //reload the page
-       
+function showFruits(){
+    
+    $("#fruits").append("<img src='images/"+chooseFruit[Math.round(8*Math.random())]+".png'>");
+    $("#fruits").css({'top' : -50, "left" : 300});
+    
+}
